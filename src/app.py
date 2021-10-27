4e3e85d592cd0797bba991c0f151942a8a29793f
Mateus Pinto da Silva <mateusps10@outlook.com>
2021-10-27 13:31:33 -0300

    tasks CR

--- src/app.py
+++ src/app.py
@@ -28,60 +28,80 @@ def close_connection(_):
 def user__register():
     DB_CUR = get_database().cursor()
     if list(DB_CUR.execute(f'SELECT COUNT(*) FROM user WHERE email="{request.form["email"]}"'))[0][0] == 1:
-        return json.dumps({'info': 'Email ja cadastrado!'}), 500, {'ContentType': 'application/json'}
+        return {'error': 1, 'error_message': 'Erro! Email já cadastrado!'}
     else:
         DB_CUR.execute(f'INSERT INTO user VALUES ("{request.form["email"]}", "{md5(request.form["password"].encode()).hexdigest()}", "{request.form["security_question"]}", "{md5(request.form["security_answer"].encode()).hexdigest()}", "{request.form["name"]}", {int(request.form["birthday_day"])}, {int(request.form["birthday_month"])}, {int(request.form["birthday_year"])})')
         get_database().commit()
 
-        return json.dumps({'info': 'Cadastrado com sucesso!'}), 200, {'ContentType': 'application/json'}
+        return {'error': 0}
 
 
 @APP.route('/user/login', methods=['POST'])
 def user__login():
     DB_CUR = get_database().cursor()
     if 'email' in session:
-        return json.dumps({'info': f'Erro! Ja logado como {session["email"]}'}), 200, {'ContentType': 'application/json'}
-
-    if list(DB_CUR.execute(f'SELECT COUNT(*) FROM user WHERE email="{request.form["email"]}" AND password="{md5(request.form["password"].encode()).hexdigest()}"'))[0][0] == 1:
-        session['email'] = request.form['email']
-        return {'info': 'Logado com sucesso!'}
+        return {'error': 1, 'error_message': f'Erro! Já logado como {session["email"]}'}
+    elif list(DB_CUR.execute(f'SELECT COUNT(*) FROM user WHERE email="{request.form["email"]}" AND password="{md5(request.form["password"].encode()).hexdigest()}"'))[0][0] == 0:
+        return {'error': 1, 'error_message': 'Erro! Email ou senha incorretos!'}
     else:
-        return json.dumps({'info': 'Erro! Email ou senha incorretos!'}), 200, {'ContentType': 'application/json'}
+        session['email'] = request.form['email']
+        return {'error': 0}
 
 
 @APP.route('/user/logout', methods=['POST'])
 def user__logout():
-    if 'email' in session:
-        session.pop('email', None)
-        return json.dumps({'info': 'Deslogado com sucesso!'}), 200, {'ContentType': 'application/json'}
+    if not 'email' in session:
+        return {'error': 1, 'error_message': 'Erro! Impossível deslogar um usuario não logado!'}
     else:
-        return json.dumps({'info': 'Erro! Impossivel deslogar um usuario nao logado!'}), 500, {'ContentType': 'application/json'}
+        session.pop('email', None)
+        return {'error': 0}
 
 
 @APP.route('/user/security/question', methods=['POST'])
 def user__security__question():
     DB_CUR = get_database().cursor()
-    if list(DB_CUR.execute(f'SELECT COUNT(*) FROM user WHERE email="{request.form["email"]}"'))[0][0] == 1:
-        return json.dumps({'info': 'Questao recuperada com sucesso!', 'security_question': list(DB_CUR.execute(f'SELECT security_question FROM user WHERE email="{request.form["email"]}"'))[0][0]}), 200, {'ContentType': 'application/json'}
+    if list(DB_CUR.execute(f'SELECT COUNT(*) FROM user WHERE email="{request.form["email"]}"'))[0][0] == 0:
+        return {'error': 1, 'error_message': 'Erro! Email não cadastrado!'}
     else:
-        return json.dumps({'info': 'Email ja cadastrado!'}), 500, {'ContentType': 'application/json'}
+        return {'error': 0, 'reponse': {'security_question': list(DB_CUR.execute(f'SELECT security_question FROM user WHERE email="{request.form["email"]}"'))[0][0]}}
 
 
 @APP.route('/user/security/answer', methods=['POST'])
 def user__security__answer():
     DB_CUR = get_database().cursor()
     if list(DB_CUR.execute(f'SELECT COUNT(*) FROM user WHERE email="{request.form["email"]}"'))[0][0] == 0:
-        return json.dumps({'info': 'Erro! Email nao cadastrado!'}), 500, {'ContentType': 'application/json'}
+        return {'error': 1, 'error_message': 'Erro! Email não cadastrado!'}
+    elif list(DB_CUR.execute(f'SELECT COUNT(*) FROM user WHERE email="{request.form["email"]}" and security_answer="{md5(request.form["security_answer"].encode()).hexdigest()}"'))[0][0] == 0:
+        return {'error': 1, 'error_message': 'Erro! Resposta de segurança incorreta!'}
+    else:
+        DB_CUR.execute(f'UPDATE user SET password="{md5(request.form["password"].encode()).hexdigest()}" WHERE email="{request.form["email"]}"')
+        get_database().commit()
+        return {'error': 0}
 
-    if list(DB_CUR.execute(f'SELECT COUNT(*) FROM user WHERE email="{request.form["email"]}" and security_answer="{md5(request.form["security_answer"].encode()).hexdigest()}"'))[0][0] == 0:
-        return json.dumps({'info': 'Erro! Email nao cadastrado!'}), 500, {'ContentType': 'application/json'}
+
+@APP.route('/task/create', methods=['POST'])
+def task__create():
+    DB_CUR = get_database().cursor()
+    if not 'email' in session:
+        return {'error': 1, 'error_message': 'Erro! Impossível cadastrar uma tarefa para um usuario não logado!'}
+    else:
+        DB_CUR.execute(f'INSERT INTO task (user_email, title, description, target_day, target_month, target_year, priority, status) VALUES ("{session["email"]}", "{request.form["title"]}", "{request.form["description"]}", {int(request.form["target_day"])}, {int(request.form["target_month"])}, {int(request.form["target_year"])}, {int(request.form["priority"])}, 0)')
+        get_database().commit()
+        return {'error': 0}
 
 
-@APP.route('/', methods=['GET'])
+@APP.route('/task/get', methods=['POST'])
+def task__get_all():
+    DB_CUR = get_database().cursor()
+    if not 'email' in session:
+        return {'error': 1, 'error_message': 'Erro! Impossível cadastrar uma tarefa para um usuario não logado!'}
+    else:
+        return {'error': 0, 'reponse': {'tasks': list({'title': x[0], 'description': x[1], 'target_day': x[2], 'target_month': x[3], 'target_year': x[4], 'priority': x[5], 'status': x[6]} for x in list(DB_CUR.execute(f'SELECT title, description, target_day, target_month, target_year, priority, status FROM task WHERE user_email="{session["email"]}"')))}}
+
+
+@APP.route('/', methods=['GET', 'POST'])
 def index():
-    if 'email' in session:
-        return json.dumps({'info': f'Logged in as {session["email"]}'}), 200, {'ContentType': 'application/json'}
-    return json.dumps({'info': 'usuario nao encontrado'}), 500, {'ContentType': 'application/json'}
+    return {'error': 0, 'response': {'session': dict(session)}}
 
 
 if __name__ == '__main__':
